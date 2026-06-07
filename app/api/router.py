038da2bd2cb9c55bd7202f6e624c936880972c43
aayushaaj <aayushaaaa.i@gmail.com>
2026-06-07 14:07:30 +0545

    updates

--- app/api/router.py
+++ app/api/router.py
@@ -5,7 +5,7 @@ from app.api.routes import documents
 from app.api.routes import flashcards
 
 api_router = APIRouter()
-api_router.include_router(questions.router, prefix="/questions", tags=["questions"])
+# api_router.include_router(questions.router, prefix="/questions", tags=["questions"])
 api_router.include_router(paragraphs.router, prefix="/paragraphs", tags=["paragraphs"])
 api_router.include_router(documents.router, prefix="/documents", tags=["documents"])
 api_router.include_router(flashcards.router, prefix="/flashcards", tags=["flashcards"])
